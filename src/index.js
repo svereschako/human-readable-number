e1fb84bc5a6a864a3d35c9ac2a011f1009e82469
svereschako <33933400+svereschako@users.noreply.github.com>
2022-01-30 20:09:29 +0200

    feat: add function "toReadable"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+  var num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  if (number < 20) 
+  	return num[number];
+  var digit = number%10;
+  if (number < 100) 
+  	return tens[Math.floor(number/10)-2] + (digit? " " + num[digit]: "");
+  if (number < 1000) 
+  	return num[Math.floor(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+  return toReadable(Math.floor(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
 }
